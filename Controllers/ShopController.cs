012d46c2fd045e71d6dfb0b2dc9de8891709dd92
Marvin <mj03505@georgiasouthern.edu>
2019-07-15 22:19:14 -0400

    Updated files
    Starting to use github desktop application. This is the updated files that I have not commited yet.

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Wicked_Candles.Models;
 
 namespace Wicked_Candles.Controllers
 {
     public class ShopController : Controller
     {
+        Wicked_CandlesEntities2 db = new Wicked_CandlesEntities2();
+
         // GET: Shop
         public ActionResult Index()
         {
-            return View();
+            return View(db.Products.ToList());
         }
 
         // GET: Product Details
